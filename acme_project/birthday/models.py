406d1e09534394b53a360ee5ac91843d2c1d3fc1
Alex Melnikov <coyotete21@gmail.com>
2026-03-29 22:06:05 +0300

    moded

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,10 +1,23 @@
 # birthday/models.py
 from django.db import models
+from django.urls import reverse
+
+# Импортируется функция-валидатор.
+from .validators import real_age
 
 
 class Birthday(models.Model):
     first_name = models.CharField('Имя', max_length=20)
     last_name = models.CharField(
-        'Фамилия', blank=True, help_text='Необязательное поле', max_length=20
+        'Фамилия', max_length=20, help_text='Опциональное поле', blank=True
     )
-    birthday = models.DateField('Дата рождения')
+    birthday = models.DateField('Дата рождения', validators=(real_age,))
+    image = models.ImageField('Фото', upload_to='birthdays_images', blank=True)
+
+
+class Meta:
+        ...
+
+    def get_absolute_url(self):
+        # С помощью функции reverse() возвращаем URL объекта.
+        return reverse('birthday:detail', kwargs={'pk': self.pk})
